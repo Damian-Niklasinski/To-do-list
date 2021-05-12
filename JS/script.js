e39e5789d511bc53e82743d722f0174ce6fc4236
Damian Niklasiński <damianiklasinski@wp.pl>
2021-05-12 20:22:43 +0200

    fixed the text-decoration and js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 {
     const tasks = [
-       
+
     ];
 
     const addNewtask = (newTaskContent) => {
@@ -13,18 +13,18 @@
 
     const removeTask = (taskIndex) => {
         tasks.splice(taskIndex, 1);
-                render();
+        render();
     };
 
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     }
-    
+
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove")
-        
+
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
@@ -32,7 +32,7 @@
         });
 
         const toggleDoneButtons = document.querySelectorAll(".js-done")
-        
+
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
@@ -45,22 +45,20 @@
 
         for (const task of tasks) {
             htmlString += `  
-                <li
-                      class="list__item${task.done ? " list__item--done" : " list__item--remove"}"                     
-                    >
-                    <button class="list__button list__button--done js-done"><span class="list__button--span">✓</span></button>    
-                      ${task.content}
-                    <button class="list__button list__button--remove js-remove">🗑</button>    
+                <li class="list__item">
+                    <button class="list__button list__button--done js-done"><span class="${task.done ? "list__button--spanVisible" : "list__button--span"}">✓</span></button>  
+                      <span class="list__span ${task.done ? "list__span--done" : "list__span--remove"}">${task.content}</span>
+                    <button class="list__button list__button--remove js-remove">🗑</button> 
                 </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
         const removeButtons = document.querySelectorAll(".js-remove")
-        
+
         bindEvents();
     };
-    
+
 
     const focusInput = (newTask) => {
         newTask.value = "";
@@ -70,10 +68,10 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask")    
+        const newTaskContent = document.querySelector(".js-newTask")
         const newTask = newTaskContent.value.trim();
-        
-        if(!newTask) {
+
+        if (!newTask) {
             focusInput(newTaskContent);
             return;
         }
